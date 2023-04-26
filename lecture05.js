04f11b6a45280029136fe90897e471485e8a139f
FarmonovAzamat <farmonovazamat21@gmail.com>
2023-04-26 17:16:05 +0300

    add lecture 5

---
+++ lecture05.js
@@ -0,0 +1,22 @@
+// let person = {
+//     firstName: 'Azamat',
+//     lastName: 'Farmonov',
+//     age: '22',
+//     hobbies: 'programming',
+//     get:function (){
+//         return this.firstName+ " "+this.lastName
+//     }
+// }
+// console.log(person.get());
+
+// let {firstName: n, lastName: s} = person;
+// console.log(n);
+// console.log(s);
+
+// let cloneObject = {...person};
+// console.log(cloneObject);
+
+let data = new Date()
+console.log(data);
+
+//1 sec = 1000 milsec
